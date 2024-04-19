07f6647e72386e2e280df3b3622498f7e6f39fd4
yusuf sonmez <yusufsonmez951@gmail.com>
2024-04-19 23:50:06 +0300

    path redirects

--- courses/urls.py
+++ courses/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('list', views.course_list),
-    path('details', views.detail),
-    path('<category>', views.get_course_by_category)
+    path('<str:course_name>', views.detail),
+    path('category/<int:category_id>', views.get_course_by_category_id),
+    path('category/<str:category_name>', views.get_course_by_category_name)
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,22 +1,31 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
+from django.shortcuts import redirect, render
 
 # Create your views here.
+
+data = {
+    'programlama' : 'Pragramlama Kurslarının Listesi',
+    'web-gelistirme' : 'Web Gelistirme Kursları Listesi',
+    'mobil' : 'Mobil Uygulama Gelistirme Kurslarının Listesi'
+}
 
 def course_list(request):
     return HttpResponse('Kurs Listesi')
 
-def detail(request):
-    return HttpResponse('Kurs Detay Sayfasi')
+def detail(request, course_name):
+    return HttpResponse(f'{course_name} Kursu Detay Sayfasi')
 
-def get_course_by_category(request, category):
-    text = ''
+def get_course_by_category_name(request, category_name):
 
-    if (category == 'programlama'):
-        text = 'Pragramlama kurslarının Listesi'
-    elif (category == 'web-gelistirme'):
-        text = 'Web Gelistirme Kursları Listesi'
-    else:
-        text = 'Geçersiz kategori girildi'
+    try:
+        text = data[category_name]
+        return HttpResponse(text)
+    except:
+        HttpResponseNotFound("Geçersiz Kategori Girildi")
 
-    return HttpResponse(text)
+def get_course_by_category_id(request, category_id):
+    category_list = list(data.keys())
+    if (category_id > len(category_list)):
+        HttpResponseNotFound("Geçersiz Kategori Girildi")
+    redirect_text = category_list[category_id - 1]
+    return redirect(f'/course/category/{redirect_text}')
